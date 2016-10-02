c212d652bd4401a8d22224f29c96191c32799e11
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-10-02 18:25:05 +0200

    initial commit

---
+++ src/webgl.js
@@ -0,0 +1,15 @@
+window.onload = () => initGL();
+
+let gl;
+
+function initGL() {
+  const canvas = document.getElementById('canvas');
+  gl = canvas.getContext('webgl');
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1, 0, 0 , 1); // rgba
+  draw();
+}
+
+function draw() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
